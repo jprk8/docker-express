fb63823b7977121e1f553a36f6933b7534cdcb66
John Park <176120986+jprk8@users.noreply.github.com>
2026-06-02 23:55:25 -0700

    Add watchtower to compose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Testing Watchtower</h1>')
+  res.send('<h1>DevOps with Docker</h1>')
 })
 
 const PORT = 8080
